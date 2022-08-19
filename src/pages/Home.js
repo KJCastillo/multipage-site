7810102a6f9c1c527765cc453e3f05b87766e354
Kevin Castillo <kevincastillo318@gmail.com>
2022-08-19 07:58:52 -0700

    custom useFetch hook added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,20 @@
-export default function home() {
+import { useFetch } from '../hooks/useFetch'
+import './Home.css'
+
+export default function Home() {
+    const { data: articles, isPending, error } = useFetch('http://localhost:3000/articles')
+
   return (
-    <div>
-      <h2>Homepage</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores, atque
-        inventore laborum cum et vitae distinctio animi enim nulla molestiae
-        cumque quaerat delectus voluptatibus facere quis sequi ratione officiis
-        suscipit?
-      </p>
+    <div className='home'>
+      <h2>Articles</h2>
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {articles && articles.map((article) => (
+        <div key={article.id} className="card">
+            <h3>{article.title}</h3>
+            <p>Written by: {article.author}</p>
+        </div>
+      ))}
     </div>
   );
 }
