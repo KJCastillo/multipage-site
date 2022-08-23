cb96e52d83f92823f51a92868a31093a0cc67b08
Kevin Castillo <kevincastillo318@gmail.com>
2022-08-23 07:18:51 -0700

    query paramater

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,15 @@
+import { useLocation } from "react-router-dom";
+
 export default function Contact() {
+  const queryString = useLocation().search
+  //get query string from the path in the browswer
+
+  const queryParams = new URLSearchParams(queryString)
+  const name = queryParams.get("name")
+
   return (
     <div>
-      <h2>Contact page</h2>
+      <h2>Hey {name}, Contact page</h2>
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est ipsa, vel
         provident hic ullam quo dignissimos distinctio? Neque reprehenderit,
